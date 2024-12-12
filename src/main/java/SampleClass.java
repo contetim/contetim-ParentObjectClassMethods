9f0146e3ce054bbc2b74cceba27521b853e6fd2c
Tim C <knb143@my.utsa.edu>
2024-12-12 22:15:10 +0000

    lab completed

--- src/main/java/SampleClass.java
+++ src/main/java/SampleClass.java
@@ -63,9 +63,17 @@ public class SampleClass {
     public boolean b;
 
     //    implement a custom .equals(SampleClass other){} method here.
-
+    public boolean equals(SampleClass other){
+        if(this.a == other.a && this.b == other.b){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 
     //    implement a custom .toString(){} method here.
-
-    
+    public String toString(){
+        return ("" + a + " " + b);
+    }
 }
